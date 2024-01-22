42ce713a511457708f22e977a9be6eeb81d97eb5
IPetrovRed <ipetrovdat@gmail.com>
2024-01-22 20:47:07 +0300

    second commit

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, Hexlet and just check of git!");
+        System.out.println("Hello, Hexlet and just check of git thing at 22.01.2024!");
     }
 }
